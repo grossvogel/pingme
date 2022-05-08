93b7f6fc6e12c0c56aa764168d4b8346435df62c
Jason Pollentier <jason.pollentier@revelry.co>
2022-05-08 11:00:01 -0500

    fix icon font setup for release

--- lib/pingme_web/endpoint.ex
+++ lib/pingme_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule PingmeWeb.Endpoint do
     at: "/",
     from: :pingme,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images css favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
